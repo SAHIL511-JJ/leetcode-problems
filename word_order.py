a6407ea6d8328037a6cd65379dadd0fad9519607
SAHIL511-JJ <sahillamture511@gmail.com>
2025-11-29 23:20:07 +0530

    Add solution for Word Order

---
+++ word_order.py
@@ -0,0 +1,14 @@
+from collections import OrderedDict
+
+def word_order(words):
+    od = OrderedDict()
+    for word in words:
+        od[word] = od.get(word, 0) + 1
+    
+    print(len(od))
+    print(*od.values())
+
+if __name__ == '__main__':
+    n = int(input())
+    words = [input() for _ in range(n)]
+    word_order(words)
