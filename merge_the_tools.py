204439bb88bf7ace25721666d2b3907d165c2dea
SAHIL511-JJ <sahillamture511@gmail.com>
2025-11-29 23:17:27 +0530

    Add solution for Merge the Tools!

---
+++ merge_the_tools.py
@@ -0,0 +1,15 @@
+def merge_the_tools(string, k):
+    n = len(string)
+    for i in range(0, n, k):
+        sub = string[i:i+k]
+        seen = set()
+        result = []
+        for char in sub:
+            if char not in seen:
+                seen.add(char)
+                result.append(char)
+        print("".join(result))
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
